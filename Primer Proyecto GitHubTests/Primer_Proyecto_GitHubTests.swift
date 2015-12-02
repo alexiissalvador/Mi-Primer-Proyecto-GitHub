b71cb50a2e62732c6d6cdb7b0202bdc5c5a9f732
Alexis Arciniega <alexiissalvador@alexiss-mac.local>
2015-12-01 19:05:11 -0600

    Initial Commit

---
+++ Primer Proyecto GitHubTests/Primer_Proyecto_GitHubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Primer_Proyecto_GitHubTests.swift
+//  Primer Proyecto GitHubTests
+//
+//  Created by Alexis Arciniega on 12/1/15.
+//  Copyright © 2015 Alexis Arciniega. All rights reserved.
+//
+
+import XCTest
+@testable import Primer_Proyecto_GitHub
+
+class Primer_Proyecto_GitHubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
